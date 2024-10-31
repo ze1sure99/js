28886ddf22a4d99a24b07458b908f0aa24082c6f
zhaozixu <1306445936@qq.com>
2024-10-31 11:49:46 +0800

    2024-10-31 11.49

---
+++ es6/c.js
@@ -0,0 +1,23 @@
+export var a = 1;
+
+export let fn = ()=>{
+
+}
+export function fn1(){
+
+}
+
+
+
+ function sum(){
+   console.log("sum")
+}
+let b =2 ;
+
+
+export {b,sum}
+
+
+function box(){
+}
+export default box;
---
+++ es6/d.js
@@ -0,0 +1,4 @@
+
+import {sum,b} from './c.js';
+console.log(sum());
+console.log(b);
